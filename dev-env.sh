1d8364cd9abad126f84f3573159b4cea2b0c3768
Deyao Chen <chendeyao000@gmail.com>
2023-10-16 17:34:29 +0000

    repurpose env

--- dev-env.sh
+++ dev-env.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# activate venv and sets up environment variables
+# env used for dev
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) &&
-source "$SCRIPT_DIR/venv/bin/activate" &&
 export PATH="$SCRIPT_DIR/target-irace/target_runner/target/release:$PATH" &&
 export PYTHONPATH="$SCRIPT_DIR/epm" &&
 export workspaceFolder=$SCRIPT_DIR
