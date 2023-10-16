8a5cbebe07018f352e6b74ebe23ac1367a8cffe5
Deyao Chen <chendeyao000@gmail.com>
2023-10-16 17:54:08 +0000

    fix typo

--- tools/data_comparison/run_all.sh
+++ tools/data_comparison/run_all.sh
@@ -10,9 +10,12 @@ cd ../../../..
 cd tools/data_comparison/train-records/cplex_regions200/data_traintest
 bash cmds.sh
 cd ../../../..
-cd tools/data_comparison/train-records/minsta_randomk3/data_train
+cd tools/data_comparison/train-records/minisat_randomk3/data_train
 bash cmds.sh
 cd ../../../..
-cd tools/data_comparison/train-records/minsta_randomk3/data_val
+cd tools/data_comparison/train-records/minisat_randomk3/data_traintest
+bash cmds.sh
+cd ../../../..
+cd tools/data_comparison/train-records/minisat_randomk3/data_val
 bash cmds.sh
 cd ../../../..
