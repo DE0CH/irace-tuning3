b954bf5396d22f61dafd8a2fdc1bd786ab0cacff
Deyao Chen <chendeyao000@gmail.com>
2024-01-02 14:39:57 +0800

    move epm to its own repo

--- epm/epm/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from epm.experiment_utils import dependencies
-
-MAXINT = 10**10
-
-with (Path(__file__).parent / 'requirements.txt').open() as fh:
-    dependencies.verify_packages(fh.read())
