33c3fbf58977a033d2c9bc3cc5af5365310b806c
Deyao Chen <chendeyao000@gmail.com>
2024-01-09 18:35:11 +0800

    cleanup

--- dev-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# env used for dev
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) &&
-export PATH="$SCRIPT_DIR/target-irace/target_runner/target/release:$PATH" &&
-export PYTHONPATH="$SCRIPT_DIR/epm" &&
-export workspaceFolder=$SCRIPT_DIR
--- tools/extract_args.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import shlex
-
-parser = argparse.ArgumentParser()
-parser.add_argument('shell_file', type=str, help='Path to the shell file.')
-args = parser.parse_args()
-with open(args.shell_file, 'r') as f:
-    shell_file = f.read()
-print(' '.join(shlex.split(shell_file)[1:]))
